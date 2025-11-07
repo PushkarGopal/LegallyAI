faa8975e8e409b3c7842ac66c3eae74b57b0d01b
Pushkar Gopal 24210077 <24210077@iitgn.ac.in>
2025-11-07 10:47:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2757243045-daec1",
+  "appId": "1:923657323581:web:29cf8f6900897c9c465654",
+  "apiKey": "AIzaSyDji78jN7Fi8IkBmpQUkBtyVtsIE7H1fAg",
+  "authDomain": "studio-2757243045-daec1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "923657323581"
+};
